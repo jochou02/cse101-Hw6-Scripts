0b1e348e046006332d1b004d95e0a5ec5782ccc4
jochou02 <jochou02@gmail.com>
2022-12-07 15:56:44 -0800

    problem4 alg working

--- problem4.py
+++ problem4.py
@@ -1,15 +1,5 @@
 import csv
 
-class Streetlamp():
-
-    def __init__(self, left, right, cost):
-        self.left = int(left) - 1
-        self.right = int(right) - 1
-        self.cost = int(cost)
-
-    def __repr__(self):
-        return f'Left: {self.left} Right: {self.right} Cost: {self.cost}'
-
 with open(f'./Script Outputs/problem4.txt', 'w') as outputFile:
 
     for index in range(100): 
@@ -25,8 +15,8 @@ with open(f'./Script Outputs/problem4.txt', 'w') as outputFile:
             lamps = []
             for line in file:
                 split = line.split(',')
-                lamp = Streetlamp(split[0], split[1], split[2])
-                lamps.append(lamp)
+                split[2] = split[2][:-1]
+                lamps.append(split)
             
             # Initialize array + bases cases
             dp = [[None for i in range(2000)] for j in range(len(lamps))]
@@ -39,14 +29,13 @@ with open(f'./Script Outputs/problem4.txt', 'w') as outputFile:
             for i in range(1, len(lamps)): # lamps
                 for j in range(1, 2000): # houses
                     # lamp i CANNOT illuminate house j
-                    if lamps[i].right < j:
+                    if int(lamps[i][1]) < j:
                         dp[i][j] = float('inf')
-                    elif lamps[i].left > j:
+                    elif int(lamps[i][0]) > j:
                         dp[i][j] = dp[i - 1][j]
                     # lamp i CAN illuminate house j
                     else:
-                        
-                        dp[i][j] = min(dp[i - 1][j], lamps[i].cost + dp[i - 1][lamps[i].left - 1]) 
+                        dp[i][j] = min(dp[i - 1][j], int(lamps[i][2]) + dp[i - 1][int(lamps[i][0]) - 1]) 
                 
             outputFile.write(f'Solution for input # {index}: {dp[-1][-1]}\n')
 
